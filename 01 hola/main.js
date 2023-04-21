812d250bb0bc6bd943964e664a2b06ac8010194b
LAEL <lael@gmail.com>
2023-04-21 12:18:11 -0500

    soy

---
+++ 01 hola/main.js
@@ -0,0 +1,10 @@
+const hola= ()=>{
+    let nombre=prompt("Cual es tu nombre?")
+    alert(" hola " + nombre + "🛐");
+  }
+  const suma=()=>{
+    let numero1 = parseInt(prompt("Número 1:"));
+    let numero2 = parseInt(prompt("Número 2:"));
+    let suma = numero1 + numero2;
+    alert("Suma: " + suma);
+  }
---
+++ 04 cambiar-imagen/app.js
@@ -0,0 +1,12 @@
+const uno=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="img1.jpeg";
+  }
+  const dos=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="img2.jpeg";
+  }
+  const tres=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="img3.jpeg";
+  }
